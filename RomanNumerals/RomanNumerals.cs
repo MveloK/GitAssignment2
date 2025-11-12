8eb8dbff66e7f38c0798586f6fe7fced67cec1fb
MveloSK <mvelokhumalo55@gmail.com>
2025-11-12 21:57:50 +0200

    First test failed

---
+++ RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,10 @@
+﻿namespace RomanNumerals
+{
+    public class RomanNumerals
+    {
+        public static int Converter
+        {
+            get { return 1 * 2; }
+        }
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using RomanNumerals; 
+
+namespace RomanNumeralsTest
+{
+    public class RomanNumeralsTest
+    {
+        [Fact]
+        public void Roman_Numeral_1()
+        {
+           
+            var result = RomanNumerals.RomanNumerals.Converter;
+
+            Assert.Equal("I", result.ToString());
+        }
+    }
+}
