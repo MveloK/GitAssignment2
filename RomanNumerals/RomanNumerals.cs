6b5e0fe4bfd4946465f7d201004b81b2afd2e8ed
MveloSK <mvelokhumalo55@gmail.com>
2025-11-18 00:46:04 +0200

    Combining rule test passed

--- RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals.cs
@@ -3,25 +3,24 @@
     public class RomanNumerals
     {
         private static readonly List<(string Numeral, int Value)> NumeralList = new List<(string Numeral, int Value)>
-{
-                            ("I", 1),
-                            ("II", 2), 
-                            ("III", 3), 
-                            ("V", 5),
-                            ("X", 10),
-                            ("XL", 40),
-                            ("L", 50),
-                            ("XC", 90),
-                            ("C", 100),
-                            ("CD", 400),
-                            ("D", 500),
-                            ("CM", 900),
-                            ("M", 1000)
-                            
-};
+        {
+            ("I", 1),
+            ("II", 2),
+            ("III", 3),
+            ("V", 5),
+            ("X", 10),
+            ("XL", 40),
+            ("L", 50),
+            ("XC", 90),
+            ("C", 100),
+            ("CD", 400),
+            ("D", 500),
+            ("CM", 900),
+            ("M", 1000)
+        };
+
         public static string Converter(int num)
         {
-            
             if (num <= 0)
                 throw new ArgumentException("False");
 
@@ -33,7 +32,23 @@
                 return roman.Numeral;
 
             var result = "";
-            var valuesDesc = NumeralList.OrderByDescending(n => n.Value);
+
+            var valuesDesc = new List<(string Numeral, int Value)>
+            {
+                ("M", 1000),
+                ("CM", 900),
+                ("D", 500),
+                ("CD", 400),
+                ("C", 100),
+                ("XC", 90),
+                ("L", 50),
+                ("XL", 40),
+                ("X", 10),
+                ("IX", 9),
+                ("V", 5),
+                ("IV", 4),
+                ("I", 1)
+            };
 
             foreach (var item in valuesDesc)
             {
--- RomanNumeralsTest/obj/Debug/net9.0/RomanNumeralsTest.AssemblyInfo.cs
+++ RomanNumeralsTest/obj/Debug/net9.0/RomanNumeralsTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RomanNumeralsTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021144b8781717552339c751a9d698f8204ecd0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934854a6a509d1204d5e4e860dddf22dd0fe2c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("RomanNumeralsTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RomanNumeralsTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
